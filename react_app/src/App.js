571dff3e0c4037bf7a5adbdff13556acdd6f97b4
Sergio Ribeiro <ssribeiro@gmail.com>
2021-08-22 17:13:09 -0300

    appComponent

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,10 +1,24 @@
+// Main import of React
+import React from 'react';
+// Theme importv #TODO
+// import { ThemeProvider } from 'styled-components';
+// import { theme } from './theme'
 import './App.css';
+
+// All Components import
+import { Header } from './components/common';
+
 
 function App() {
   return (
-    <div className="App">
-      hi
-    </div>
+    // < ThemeProvider theme={theme}>
+      <div className="App">
+        <Header />
+        <div className="main">
+
+        </div>
+      </div>
+    // </ThemeProvider>
   );
 }
 
