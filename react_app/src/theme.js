e3522136d3d21c79f5c6c0bfbb63f439f46fd185
Sergio Ribeiro <ssribeiro@gmail.com>
2021-08-22 17:36:03 -0300

    protoThemeNotReady

---
+++ react_app/src/theme.js
@@ -0,0 +1,11 @@
+// App Theme
+export const theme = {
+    // Enter here important style variables
+    primaryDark: '#0D0C1D',
+    // Enter here important style variables
+    primaryLight: '#EFFFFA',
+    // Enter here important style variables
+    primaryHover: '#343078',
+    // Enter here important style variables
+    mobile: '576px', 
+}
