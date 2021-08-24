c787eaca17cd70fd6b70ca433a6bd794ae99cde7
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-08-24 16:44:30 -0300

    Navbar

--- react_app/src/components/common/navbar/Navbar.jsx
+++ react_app/src/components/common/navbar/Navbar.jsx
@@ -1,22 +1,44 @@
 // # Main Import
-import React from 'react';
+import React, { useState } from 'react';
+
+// # Main SubComponents
+import { Menu } from '../../common';
 
 // # Import Component Style
 import './Navbar.css'
 
 // Navbar React Component Construction
 function Navbar () {
+
+    // Saves current state
+    // state is an array:
+    const [navLinkOpen, navLinkToggle] = useState(false);
+        // Equivalent to:
+        // const state = useState(false);
+        // const navLinkOpen = state [0]
+        // const navLingToggle = state [1]
+        
+    // Define the Callback for the toggle
+    const hangleNavLinksToggle = () => {
+        navLinkToggle(!navLinkOpen)
+    };
+
     return (
         // Navbar
-        <nav className="navbar">
+        <nav className="navbar" >
             {/* Navbar Link tree */}
             <ul className="navlinks">
                 {/* Items: Replace for mapping */}
-                <li className="navbar-item" > Home </li>
-                <li className="navbar-item" > About </li>
-                <li className="navbar-item" > Control </li>
+                <li className="navbar-item"> <a href="\" >Home</a> </li>
+                <li className="navbar-item"> <a href="\" >About</a> </li>
+                <li className="navbar-item"> <a href="\" >Control</a> </li>
+                <li className="navbar-item"> <a href="\" >Control</a> </li>
             </ul>
+            <div className="navmenu">
+                <Menu />
+            </div>
         </nav>
+        
     )
 
 }
