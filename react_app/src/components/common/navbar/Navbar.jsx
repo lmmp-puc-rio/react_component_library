69bff3ffa697f00130a2ab34d9a1046daff8d8d3
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-08-25 13:58:33 -0300

    NavbarMenuAnimation

--- react_app/src/components/common/navbar/Navbar.jsx
+++ react_app/src/components/common/navbar/Navbar.jsx
@@ -11,30 +11,43 @@ import './Navbar.css'
 function Navbar () {
 
     // Saves current state
-    // state is an array:
+    // state is an array composed of:
+    //    [Variable   ,  Method that sets the variable] = useState(defaultValue)
     const [navLinkOpen, navLinkToggle] = useState(false);
         // Equivalent to:
         // const state = useState(false);
-        // const navLinkOpen = state [0]
-        // const navLingToggle = state [1]
-        
-    // Define the Callback for the toggle
-    const hangleNavLinksToggle = () => {
+        // const navLinkOpen = state[0]
+        // const navLingToggle = state[1]
+
+    // Defines the Callback for the toggle
+    function hangleNavLinksToggle () {
+        // Calls the Function to set the Variable value to the oposit of what it was before
         navLinkToggle(!navLinkOpen)
     };
+
+    function renderClasses () {
+        // set the navlinks class as targed
+        let classes = "navlinks"
+
+        if (navLinkOpen) {
+            // add active to navlinks class
+            classes += " active"
+        }
+        return classes
+    }
 
     return (
         // Navbar
         <nav className="navbar" >
             {/* Navbar Link tree */}
-            <ul className="navlinks">
+            <ul className={renderClasses()}>
                 {/* Items: Replace for mapping */}
                 <li className="navbar-item"> <a href="\" >Home</a> </li>
                 <li className="navbar-item"> <a href="\" >About</a> </li>
                 <li className="navbar-item"> <a href="\" >Control</a> </li>
                 <li className="navbar-item"> <a href="\" >Control</a> </li>
             </ul>
-            <div className="navmenu">
+            <div onClick={hangleNavLinksToggle} className="navmenu">
                 <Menu />
             </div>
         </nav>
