a67828ac154b698c8f722c309281a7e839a84f4f
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-08-27 14:57:43 -0300

    GridFormComponent

--- react_app/src/components/common/gridForm/GridForm.jsx
+++ react_app/src/components/common/gridForm/GridForm.jsx
@@ -1,24 +1,30 @@
 // # Main Import 
-import React from 'react';
+import React, { useState } from 'react';
+import Logo from '../logo';
 
 // # Import Component Style
 import './GridForm.css'
 
 // # GrifForm Row React Component Construction
-function GridFormRow () {
+function GridFormInput (props) {
     
+    // Initialize variable states:
+    // const [input, setInput] = props.input
+
     return (
         // Logo assembled by Icon and Name
-            <form className="gridFormRow">
-                <div className="rowLabel">
-                    <label>
-                        input 1
+            <form >
+                <div className="gridFormInput">
+                    <label className="rowLabel">
+                        {props.name}
                     </label>
-                </div>
-                <div  className="rowInput">
-                    <input 
+                    {/* Important to set input tag name property */}
+                    <input className="rowInput"
+                        name={props.name}
                         type="text"
                         required
+                        value= {props.value}
+                        onChange={(e) => props.onChange(e)}
                     />
                 </div>
             </form>
@@ -28,11 +34,23 @@ function GridFormRow () {
 
 // # GrifForm Row React Component Construction
 function GridForm () {
+    // const defaultValues = [
+        // {name: "test2", value: "2"}];
+
+    const [state, setState] = useState({text1: "teste1" , text2: "teste2"})
     
+    function onChangeCallback(event) {
+        const { name , value } = event.target;
+
+        setState(prevState => ({ ...prevState, [name]: value}));
+    }
+
     return (
         // Logo assembled by Icon and Name
-        <div className="gridFormRow" >
-            <GridFormRow />
+        <div className="gridFormSection" >
+            <h4> Section Title</h4>
+            <GridFormInput name={"text1"} value={state.text1} onChange={onChangeCallback} />
+            <GridFormInput name={"text2"} value={state.text2} onChange={onChangeCallback} />
         </div>
     )
 
