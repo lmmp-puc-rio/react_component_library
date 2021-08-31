435a286a6b1d300b6854674da562e8296d5fef38
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-08-31 09:37:02 -0300

    tagUniqueKeys&NewInputData

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -18,18 +18,20 @@ function App() {
     {url: "/" , title: "Control", className: "navbar-item"},
     {url: "/" , title: "Help", className: "navbar-item"}]
     
-    const conversionFactors = { lenght: {"km": 10^-3, "mm": 10^3, "um": 10^6, "m": 1},
+    //TODO: Bring from the Flask API
+    const conversionFactors = { lenght: {"km": 0.001, "mm": 1000, "um": 1000000, "m": 1},
                                 pressure: {"psi": 0.000145038, "mmHg": 0.00750062, "Pa": 1,}};
     
     const  data = {gap: {label: "Gap:", defaultValue:"0", unitType:"lenght"},
-                  size: {label: "Size:", defaultValue:"0", unitType:"lenght"}};
+                  size: {label: "Size:", defaultValue:"0", unitType:"lenght"},
+                  p0: {label: "Inlet Pressure:", defaultValue:"0", unitType:"pressure"}};
   return (
     // < ThemeProvider theme={theme}>
-      <div className="App">
+      <div name="app" className="App">
         <Header navlinks={navlinks} mail={"info@difsolutions.com"} />
-        <div className="main">
-          <div className="scope">
-              <GridForm data={data} conversionFactors={conversionFactors}/>
+        <div name="main" className="main">
+          <div name="scope" className="scope">
+              <GridForm data={data} conversionFactors={conversionFactors} />
           </div>
         </div> 
       </div>
