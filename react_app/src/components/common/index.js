62a74e8f14bfc33c609d18cd95f7602f163c957d
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-09-01 10:28:55 -0300

    commonExport

--- react_app/src/components/common/index.js
+++ react_app/src/components/common/index.js
@@ -4,6 +4,7 @@ import Logo from "./logo";
 import Menu from "./menu";
 import Dropdown from "./dropdown";
 import GridForm from "./gridForm";
+import { Container, Link, Button, darkColors, lightColors } from "./fab";
 
 
 export {
@@ -13,4 +14,9 @@ export {
     Menu,
     Dropdown,
     GridForm,
+    Container, 
+    Link, 
+    Button, 
+    darkColors, 
+    lightColors,
 }
