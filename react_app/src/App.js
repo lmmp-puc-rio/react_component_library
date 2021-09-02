508b6dc772c49aeeea64460b611a5d667c4ad792
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2021-09-02 13:03:52 -0300

    appNewComponentsUpdate

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -7,7 +7,7 @@ import './App.css';
 
 // All Components import
 import { Header, GridForm, 
-        Button, FAB, Link,
+        FAB, //ActionButton,  Action, // Last two only needed for manual creation of items
         darkColors, lightColors } from './components/common';
 
 
@@ -21,45 +21,54 @@ function App() {
     {url: "/" , title: "Control", className: "navbar-item"},
     {url: "/" , title: "Help", className: "navbar-item"}]
     
-    //TODO: Bring from the Flask API
-    const conversionFactors = { lenght: {"km": 1000, "cm": 1/100, "mm": 1/1000, "um": 1/1000000, "m": 1},
-                                pressure: {"psi": 0.000145038, "mmHg": 0.00750062, "Pa": 1,}};
-    
-    const  data = {gap: {label: "Gap:", defaultValue:"0", unitType:"lenght", selectecUnit:"mm"},
-                  size: {label: "Size:", defaultValue:"0", unitType:"lenght", selectecUnit:"cm"},
-                  p0: {label: "Inlet Pressure:", defaultValue:"0", unitType:"pressure", selectecUnit:"psi"}};
+  //TODO: Bring all dynamic data from the Flask API
+  const conversionFactors = { lenght: {"km": 1000, "cm": 1/100, "mm": 1/1000, "um": 1/1000000, "m": 1},
+                              pressure: {"psi": 0.000145038, "mmHg": 0.00750062, "Pa": 1,}};
+  
+  const  formData = {gap: {label: "Gap:", defaultValue:"0", unitType:"lenght", selectecUnit:"mm"},
+                size: {label: "Size:", defaultValue:"0", unitType:"lenght", selectecUnit:"cm"},
+                p0: {label: "Inlet Pressure:", defaultValue:"0", unitType:"pressure", selectecUnit:"psi"}};
+  
+  const actionData = {actionButtom: {tootip:"Grupo de Ações" ,icon:"fas fa-plus" ,rotate:true, 
+                                      backgroundColor: darkColors.blue ,color: lightColors.white},
+                      actions: [{tooltip:"Ação 0" ,icon:"fas fa-react" ,key:"action1" ,url:"/" ,backgroundColor: darkColors.blue ,color: lightColors.white},
+                                {tooltip:"Ação 1" ,icon:"fas fa-minus" ,key:"action2" ,url:"/" ,backgroundColor: darkColors.blue ,color: lightColors.white}
+                                ]};
   return (
     // < ThemeProvider theme={theme}>
       <div name="app" className="App">
         <Header navlinks={navlinks} mail={"info@difsolutions.com"} />
         <div name="main" className="main">
           <div name="scope" className="scope">
-              <GridForm data={data} conversionFactors={conversionFactors} />
+              {/* AUTOMATIC CREATION FROM DATA: simply pass data prop */}
+              <GridForm data={formData} conversionFactors={conversionFactors} />
           </div>
         </div>
-        {/*TODO: Bring link and buttom inside FAB and populate from object data */}
-        <FAB>
-          <Link 
+        
+        {/* AUTOMATIC CREATION FROM DATA: simply pass data prop */}
+        <FAB data={actionData} />
+        
+        {/* MANUAL CREATION: insert n Actions and 1 ActionButtom Components */}
+        {/* <FAB>
+          <Action 
             tooltip={"Ação 0"}
             icon="fas fa-minus" 
             href="/baaah" 
             styles={{backgroundColor: darkColors.blue, 
                     color: lightColors.white}}/>
-          <Link
-            tooltip={"Ação 0"}
+          <Action
+            tooltip={"Ação 1"}
             icon="fas fa-react" 
             href="/baaah" 
             styles={{backgroundColor: darkColors.blue, 
                 color: lightColors.white}}/>
-          <Button
-            tooltip={"Ação 0"}
+          <ActionButton
+            tooltip={"Grupo de Ações"}
             icon="fas fa-plus"
             rotate={true}
             styles={{backgroundColor: darkColors.blue, 
-              color: lightColors.white}}>
-              
-          </Button>
-        </FAB> 
+              color: lightColors.white}}/>
+        </FAB>  */}
       </div>
     // </ThemeProvider>
   )
