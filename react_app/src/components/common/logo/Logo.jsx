c999f81d44ad56b751a06cdf55322b7b2642e742
Vinicius Mattoso <vinicius.vmrs@gmail.com>
2022-09-13 15:48:02 -0300

    [update] Logo.jsx : include image options

--- react_app/src/components/common/logo/Logo.jsx
+++ react_app/src/components/common/logo/Logo.jsx
@@ -6,16 +6,30 @@ import './Logo.css'
 
 // # Logo React Component Construction
 function Logo (props) {
-    
-    return (
-        // Logo assembled by Icon and Name
-        <a className="logo" href={props.url}>
-            {/* Logo Icon */}
-            <i className={props.iconClassName}> </i>
-            {/* Logo Name */}
-            <h4>{props.text}</h4>
-        </a>
-    )
+    if (props.image_path === 'undefined' || props.image_path === 'Null' || props.image_path === '') {
+        return (
+            // Logo assembled by Icon and Name
+            <a className="logo" href={props.url}>
+                {/* Logo Icon */}
+                <i className={props.iconClassName}> </i>
+                {/* Logo Name */}
+                <h4>{props.text}</h4>
+            </a>
+        )
+    }
+    else {
+        return (
+            // Logo assembled by Icon and Name
+            <a className="logo" href={props.url}>
+                {/* Logo Icon */}
+                <img src={props.image_path} class="logo-image" />
+                {/* Logo Name */}
+                <div style={{ width: "30px" }}></div>
+                <h4>{"       " + props.text}</h4>
+            </a>
+
+        )
+    }
 
 }
 
