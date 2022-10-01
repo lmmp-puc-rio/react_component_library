80ecf0e6a2f7606e6f60ac528382285dea5a3370
Sergio Ribeiro <sergio@lmmp.mec.puc-rio.br>
2022-10-01 07:09:53 -0300

    [update] modal component

---
+++ react_app/src/components/common/modal/Modal.jsx
@@ -0,0 +1,64 @@
+// # Main Import
+import React, { useContext } from 'react';
+import ReactDOM from 'react-dom';
+import { Link } from 'react-router-dom';
+
+// # Import Component Style
+import './Modal.css'
+
+//Import Specific Styling
+import {darkColors} from '../MaterialColors'
+
+// # Contexts
+import { ModalContext } from '../../contexts/ModalContext';
+
+// add this states to the main component where modal appears
+// const [isModalOpen,setIsModalOpen] = useState(true);
+// Add children to define message inside each modal
+// Props: isOpen, setIsOpen and confirmAction(callback for confirm button)
+
+
+function Modal(props) {
+
+    const {isModalOpen, setIsModalOpen} = useContext(ModalContext)
+    const cancelURL = props.cancelURL;
+
+    const closeModal = () => {
+        setIsModalOpen(!isModalOpen);
+        console.log(isModalOpen)
+    }
+
+    if (!isModalOpen) return null;
+
+    return ReactDOM.createPortal(
+        <div>
+            {/* Dark background thar covers screen */}
+            <div className="modal_overlay">
+                {/* Main container */}
+                <div className='modal_container'>
+                    {/* Close X button on right upper corner */}
+                    <a href={cancelURL}>
+                        <button onClick={closeModal} className='modal_close-button'><i className='fas fa-times'/></button>
+                    </a>
+                    {/* Children that defines modal internal features */}
+                    {props.children}
+                    {/* Cancel button */}
+                    <a href={cancelURL}>
+                    {/* //TODO: Check how to do without reload */}
+                        <button className='modal_cancel-button'
+                                onClick={closeModal}
+                                //specific styling
+                                style={{backgroundColor: darkColors.red}}>
+                                <i className='fas fa-times'/> Cancel
+                        </button>
+                    </a>
+                    
+                    
+                </div>
+            </div>
+        </div>
+        ,document.getElementById("modal_portal"),
+        );
+    }
+
+export default Modal;
