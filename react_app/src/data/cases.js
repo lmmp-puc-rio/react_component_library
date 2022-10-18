778c5523809bfa125cfeb31ade74bbebba23bd5e
annatrajano <annatrajano95@gmail.com>
2022-10-18 13:14:33 -0300

    [add] create SearchContext to implement SearchBar component

---
+++ react_app/src/data/cases.js
@@ -0,0 +1,47 @@
+export const dataCases =  [
+  {
+    img: "https://cenariosgas.editorabrasilenergia.com.br/wp-content/uploads/sites/6/2018/09/po%C3%A7o-transparente.png",
+    title: "Caso 1",
+    description: "Projeto de cimentação do Trecho 1",
+    card_name: "card1",
+    created_name: "Teste 1",
+    modified_name: "Teste 1",
+    created_data: "01/10/2022",
+    modified_data: "18/10/2022",
+    fluids: [
+      "FPBNA 9.0 ppg",
+      "Colchão Espaçador 10 ppg",
+      "Pasta Conv. 15ppg",
+    ],
+  },
+  {
+    img: "https://cenariosgas.editorabrasilenergia.com.br/wp-content/uploads/sites/6/2018/09/po%C3%A7o-transparente.png",
+    title: "Caso 2",
+    description: "Projeto de cimentação do Trecho 2",
+    card_name: "card2",
+    created_name: "Teste 2",
+    modified_name: "Teste 2",
+    created_data: "10/12/2022",
+    modified_data: "20/12/2022",
+    fluids: [
+      "FPBNA 9.0 ppg",
+      "Colchão Espaçador 10 ppg",
+      "Pasta Conv. 15ppg",
+    ],
+  },
+  {
+    img: "https://cenariosgas.editorabrasilenergia.com.br/wp-content/uploads/sites/6/2018/09/po%C3%A7o-transparente.png",
+    title: "Caso 3",
+    description: "Projeto de cimentação do Trecho 3",
+    card_name: "card2",
+    created_name: "Teste 3",
+    modified_name: "Teste 3",
+    created_data: "25/12/2022",
+    modified_data: "30/12/2022",
+    fluids: [
+      "FPBNA 9.0 ppg",
+      "Colchão Espaçador 10 ppg",
+      "Pasta Conv. 15ppg",
+    ],
+  }
+];
