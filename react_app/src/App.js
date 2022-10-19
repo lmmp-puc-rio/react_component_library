f76ce7d4eb009ac4f7ce1c4fa6553c5c2d986f3c
annatrajano <annatrajano95@gmail.com>
2022-10-19 10:43:54 -0300

    [add] FAB Component with dynamic direction

--- react_app/src/App.js
+++ react_app/src/App.js
@@ -1,20 +1,25 @@
 // Main import of React
-import React, {useState} from 'react';
+import React, { useState } from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 
 // Theme importv #TODO
 // import { ThemeProvider } from 'styled-components';
 // import { theme } from './theme'
-import './App.css';
+import "./App.css";
 
 // All Components import
-import { Header, SlidingPanel, SideMenu,
-        FAB, //ActionButton,  Action, // Last two only needed for manual creation of items
-        darkColors, lightColors, TabsComponent } from './components/common';
+import {
+  Header,
+  SlidingPanel,
+  SideMenu,
+  FAB, //ActionButton,  Action, // Last two only needed for manual creation of items
+  darkColors,
+  lightColors,
+  TabsComponent,
+} from "./components/common";
 
 // All Pages Import
-import { Grids, Projects, Cases, Accordions, TabsComponents} from './pages';
-
+import { Grids, Projects, Cases, Accordions, TabsComponents } from "./pages";
 
 // All Contexts Import
 // import { AuthProvider } from './components/contexts/AuthContext';
@@ -27,74 +32,104 @@ function App() {
 
   // Set navlinks Object
   const navlinks = [
-    {url: "/" , title: "Home", className: "navbar-item"},
-    {url: "/grids" , title: "Grids", className: "navbar-item"},
-    {url: "/projects" , title: "Projects", className: "navbar-item"},
-    {url: "/cases" , title: "Cases", className: "navbar-item"},
-    {url: "/accordion" , title: "Accordion", className: "navbar-item"},
-    {url: "/tabs" , title: "Tabs", className: "navbar-item"},
-    {url: "/help" , title: "Help", className: "navbar-item"}]
-    
-  const actionData = {actionButtom: {tootip:"Grupo de Ações" ,icon:"fas fa-plus" ,rotate:true, 
-                                      backgroundColor: darkColors.blue ,color: lightColors.white},
-                      actions: [{tooltip:"Ação 0" ,icon:"fas fa-react" ,key:"action1" ,url:"/" ,backgroundColor: darkColors.green ,color: lightColors.white},
-                                {tooltip:"Ação 1" ,icon:"fas fa-minus" ,key:"action2" ,url:"/" ,backgroundColor: darkColors.red ,color: lightColors.white}
-  ]};
+    { url: "/", title: "Home", className: "navbar-item" },
+    { url: "/grids", title: "Grids", className: "navbar-item" },
+    { url: "/projects", title: "Projects", className: "navbar-item" },
+    { url: "/cases", title: "Cases", className: "navbar-item" },
+    { url: "/accordion", title: "Accordion", className: "navbar-item" },
+    { url: "/tabs", title: "Tabs", className: "navbar-item" },
+    { url: "/help", title: "Help", className: "navbar-item" },
+  ];
+
+  const actionData = {
+    actionButtom: {
+      tootip: "Grupo de Ações",
+      icon: "fas fa-plus",
+      rotate: true,
+      backgroundColor: darkColors.blue,
+      color: lightColors,
+      /* Direction: Array responsible for determining the opening position of the button */
+      direction: ["fab-container-up", "fab-container-down", "fab-container-left", "fab-container-right"],
+    },
+    actions: [
+      {
+        tooltip: "Ação 0",
+        icon: "fas fa-react",
+        key: "action1",
+        url: "/",
+        backgroundColor: darkColors.green,
+        color: lightColors.white,
+      },
+      {
+        tooltip: "Ação 1",
+        icon: "fas fa-minus",
+        key: "action2",
+        url: "/",
+        backgroundColor: darkColors.red,
+        color: lightColors.white,
+      },
+    ],
+  };
+
 
   return (
     // React Browser Router
     <Router>
       <div name="app" className="App">
-        <SideMenu  onCollapse={(inactive) => {setInactive(inactive)}} />
+        <SideMenu
+          onCollapse={(inactive) => {
+            setInactive(inactive);
+          }}
+        />
         <SlidingPanel />
         <Header navlinks={navlinks} mail={"info@difsolutions.com"} />
         <div name="main" className="main">
-        <Switch>
+          <Switch>
             {/* Main page Route */}
             <Route exact path="/">
-                <div className="scope" style={{color:"white"}}>Main Page</div>
+              <div className="scope" style={{ color: "white" }}>
+                Main Page
+              </div>
             </Route>
 
             {/* Grids Route */}
             <Route exact path={navlinks[1].url}>
               <div name="inputs" className="grid-container">
-                <Grids/>
+                <Grids />
               </div>
             </Route>
             {/* Projects Route */}
             <Route exact path={navlinks[2].url}>
               <div name="cards" className="card-container">
-                <Projects/>
+                <Projects />
               </div>
             </Route>
             {/* Cases Route */}
             <Route exact path={navlinks[3].url}>
               <div name="cards" className="card-container">
-                  <Cases/>
+                <Cases />
               </div>
             </Route>
             {/* Accordion Route */}
             <Route exact path={navlinks[4].url}>
               <div name="inputs" className="grid-container">
-                <Accordions/>
+                <Accordions />
               </div>
             </Route>
 
             {/* Tabs Route */}
             <Route exact path={navlinks[5].url}>
               <div name="inputs" className="card-container">
-               <TabsComponents name="page" />
+                <TabsComponents name="page" />
               </div>
             </Route>
-
           </Switch>
         </div>
         {/* AUTOMATIC CREATION FROM DATA: simply pass data prop */}
         <FAB data={actionData} />
       </div>
     </Router>
-          
-  )
+  );
 }
 
 export default App;
