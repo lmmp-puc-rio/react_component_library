2dc9fb67ad48a1c97a401a15744edb12013a8e0b
Renan Freixo <renanfreixo@hotmail.com>
2022-10-21 16:19:22 -0300

    [update] change graph

--- react_app/src/pages/Plotly.js
+++ react_app/src/pages/Plotly.js
@@ -3,23 +3,26 @@ import Plot from "react-plotly.js";
 import './pages.css'
 const Plotly = () => {
 const graph = [
-    {x:[2,5,6],
-     y:[2,4,6],
+    {x:[0,10],
+     y:[8,8],
      //type change the mode graph
     type:"scratter",
     mode:"lines",
+    fill:'tozeroy'
     },
-    {   x:[4,8,9],
-        y:[5,8,7],
+    {   x:[6,8,7],
+        y:[5,5,7],
       //type change the mode graph
        type:"bar",
        mode:"lines",
+       fill:'tozeroy'
        },
-       {   x:[3,5,10],
-        y:[2,7,8],
+       { x:[0,10],
+         y:[3,3],
       //type change the mode graph
        type:"scatter",
-       mode:"markers",
+       mode:"line",
+       fill:'tozeroy'
        },
  ]
     return(
