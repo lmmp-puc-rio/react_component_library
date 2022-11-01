2dfad439647c54d5eb39594069fdd7e5bf40eb14
annatrajano <annatrajano95@gmail.com>
2022-11-01 08:23:32 -0300

    [fix] refactor Import Data component

--- react_app/src/components/common/importData/importData.jsx
+++ react_app/src/components/common/importData/importData.jsx
@@ -1,47 +1,43 @@
 // Import Data = Component that import json file
 
 // # Main Import
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 
 // # Import Component Style
 import "./importData.css";
 
 function ImportData() {
+/* State that loads the imported json information */
   const [files, setFiles] = useState([]);
-  const [nameFile, setNameFile] = useState([]);
+
+/* State to import errors */
+  const [error, setErrorData] = useState([])
 
 
-  const readFile = e => {
+/* Function that reads JSON files and stores in State   */
+
+  const readFile = (uploadedFile) =>{
     const fileReader = new FileReader();
-    console.log(e.target)
-    const { files } = e.target;
-    console.log(files)
-    setNameFile(files[0].name);
-    fileReader.readAsText(files[0], "UTF-8");
-    fileReader.onload = (e) => {
-      const content = e.target.result;
-      setFiles(JSON.parse(content));
-    };
-  };
-
-  const removeFile = () => {
-    setFiles([]);
-    setNameFile([]);
-}
+    fileReader.onloadend = ()=>{
+       try{
+          setFiles(JSON.parse(fileReader.result));
+          setErrorData(null)      
+        }catch(e){
+          setErrorData("**Not valid JSON file!**");
+          setFiles([])
+       }
+    }
+    if( uploadedFile !== undefined)
+       fileReader.readAsText(uploadedFile);
+ }
 
   console.log(files);
+  console.log(error);
 
   return (
     <>
       <h3>Upload Json file - Example : </h3>
-      <div className="input-container">
-        <label className="custom-file-upload">
-          <input type="file" multiple onChange={readFile} />
-          <i class="fas fa-upload" />
-          <div className="input-text">Browse..</div>
-        </label>
-        {nameFile ? <div className="file-preview" onClick={removeFile}>{nameFile}</div> : " "}
-      </div>
+        <input class="custom-file-input" type="file" onChange={(e)=>readFile(e.target.files[0])}></input>
     </>
   );
 }
