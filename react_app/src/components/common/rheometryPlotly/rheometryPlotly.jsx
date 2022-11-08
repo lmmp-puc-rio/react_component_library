52ce11265b1f1d4f20017d27bdc503d113c6dd27
annatrajano <annatrajano95@gmail.com@example.com>
2022-11-08 14:29:20 -0300

    [fix] remove rheometryPlotly.css

--- react_app/src/components/common/rheometryPlotly/rheometryPlotly.jsx
+++ react_app/src/components/common/rheometryPlotly/rheometryPlotly.jsx
@@ -3,9 +3,6 @@ import React from "react";
 import Plot from "react-plotly.js";
 
 
-// # Import Component Style
-import "./rheometryPlotly.css";
-
 function RheometryPlotly(props) {
   const data = props.data;
   const layout = props.layout;
