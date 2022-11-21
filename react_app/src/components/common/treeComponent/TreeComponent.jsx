78368fb7e5d1293b880412f86eab4ae54570f0f9
Renan Freixo <renanfreixo@hotmail.com>
2022-11-21 15:14:18 -0300

    [add] Tree CSS

--- react_app/src/components/common/treeComponent/TreeComponent.jsx
+++ react_app/src/components/common/treeComponent/TreeComponent.jsx
@@ -11,13 +11,13 @@ function TreeComponent(props) {
       }
     const name = props.name;
     const data = props.data;
-    console.log(data.name)
-    const sign = show ? <i class="fa fa-folder-open" /> :  <i class="fa fa-folder"/> 
+    const sign = show ? <i class="fas fa-folder-open"></i> :  <i class="fas fa-folder"></i> 
 
     const treeWrap = data.map((item)=>{
                   if(item.info === null) {
                     return(
                       <>
+                      <i class="far fa-folder"></i>
                       {item.name}<br/>
                       </>
                     )  
@@ -31,7 +31,7 @@ function TreeComponent(props) {
               
       <div className='TreeComponent'>
           <button onClick={toggleMenu}>
-            <span>{sign}</span>
+            {sign}
             {name}
           </button> 
           {show && treeWrap}
