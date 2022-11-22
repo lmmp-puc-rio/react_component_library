c6b44852db1bc508617c0669bbbebdba62def030
annatrajano <annatrajano95@gmail.com>
2022-11-22 13:24:19 -0300

    [add] Pump Sequence Fluids (Plotly)

---
+++ react_app/src/utils/functionsToDrawPumpSequence.js
@@ -0,0 +1,420 @@
+const pumped_fluids = [
+    {
+      color: "#9a0000",
+      points: [
+        [0, 6102],
+        [0, 6112],
+        [6.125, 6112],
+        [6.125, 6102],
+        [0, 6102],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 6102],
+        [0, 6112],
+        [-6.125, 6112],
+        [-6.125, 6102],
+        [0, 6102],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 0],
+        [0, 2059],
+        [2.5, 2059],
+        [2.5, 0],
+        [0, 0],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 0],
+        [0, 2059],
+        [-2.5, 2059],
+        [-2.5, 0],
+        [0, 0],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 2059],
+        [0, 3358],
+        [4.78, 3358],
+        [4.78, 2059],
+        [0, 2059],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 2059],
+        [0, 3358],
+        [-4.78, 3358],
+        [-4.78, 2059],
+        [0, 2059],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 3358],
+        [0, 5427],
+        [4.578, 5427],
+        [4.578, 3358],
+        [0, 3358],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 3358],
+        [0, 5427],
+        [-4.578, 5427],
+        [-4.578, 3358],
+        [0, 3358],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 5427],
+        [0, 5555],
+        [4.328, 5555],
+        [4.328, 5427],
+        [0, 5427],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 5427],
+        [0, 5555],
+        [-4.328, 5555],
+        [-4.328, 5427],
+        [0, 5427],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 5555],
+        [0, 6072],
+        [4.379, 6072],
+        [4.379, 5555],
+        [0, 5555],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [0, 5555],
+        [0, 6072],
+        [-4.379, 6072],
+        [-4.379, 5555],
+        [0, 5555],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [0, 6072],
+        [0, 6102],
+        [4.379, 6102],
+        [4.379, 6072],
+        [0, 6072],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [0, 6072],
+        [0, 6102],
+        [-4.379, 6102],
+        [-4.379, 6072],
+        [0, 6072],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [4.937499999999999, 5555],
+        [4.937499999999999, 6102],
+        [6.125, 6102],
+        [6.125, 5555],
+        [4.937499999999999, 5555],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [-4.937499999999999, 5555],
+        [-4.937499999999999, 6102],
+        [-6.125, 6102],
+        [-6.125, 5555],
+        [-4.937499999999999, 5555],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [5.375000000000001, 5527],
+        [5.375000000000001, 5555],
+        [6.125, 5555],
+        [6.125, 5527],
+        [5.375000000000001, 5527],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [-5.375000000000001, 5527],
+        [-5.375000000000001, 5555],
+        [-6.125, 5555],
+        [-6.125, 5527],
+        [-5.375000000000001, 5527],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [5.375000000000001, 5200.000000000001],
+        [5.375000000000001, 5527],
+        [6.2, 5527],
+        [6.2, 5200.000000000001],
+        [5.375000000000001, 5200.000000000001],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#9a9a9a",
+      points: [
+        [-5.375000000000001, 5200.000000000001],
+        [-5.375000000000001, 5527],
+        [-6.2, 5527],
+        [-6.2, 5200.000000000001],
+        [-5.375000000000001, 5200.000000000001],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00003",
+      name: "pasta",
+    },
+    {
+      color: "#ff5500",
+      points: [
+        [5.375000000000001, 5127],
+        [5.375000000000001, 5200.000000000001],
+        [6.2, 5200.000000000001],
+        [6.2, 5127],
+        [5.375000000000001, 5127],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00002",
+      name: "espacador",
+    },
+    {
+      color: "#ff5500",
+      points: [
+        [-5.375000000000001, 5127],
+        [-5.375000000000001, 5200.000000000001],
+        [-6.2, 5200.000000000001],
+        [-6.2, 5127],
+        [-5.375000000000001, 5127],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00002",
+      name: "espacador",
+    },
+    {
+      color: "#ff5500",
+      points: [
+        [5.375000000000001, 4783.719035861366],
+        [5.375000000000001, 5127],
+        [6.1875, 5127],
+        [6.1875, 4783.719035861366],
+        [5.375000000000001, 4783.719035861366],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00002",
+      name: "espacador",
+    },
+    {
+      color: "#ff5500",
+      points: [
+        [-5.375000000000001, 4783.719035861366],
+        [-5.375000000000001, 5127],
+        [-6.1875, 5127],
+        [-6.1875, 4783.719035861366],
+        [-5.375000000000001, 4783.719035861366],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00002",
+      name: "espacador",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [5.375000000000001, 2059],
+        [5.375000000000001, 4783.719035861366],
+        [6.1875, 4783.719035861366],
+        [6.1875, 2059],
+        [5.375000000000001, 2059],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [-5.375000000000001, 2059],
+        [-5.375000000000001, 4783.719035861366],
+        [-6.1875, 4783.719035861366],
+        [-6.1875, 2059],
+        [-5.375000000000001, 2059],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [3.3124999999999996, 0],
+        [3.3124999999999996, 2059],
+        [9.5, 2059],
+        [9.5, 0],
+        [3.3124999999999996, 0],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+    {
+      color: "#9a0000",
+      points: [
+        [-3.3124999999999996, 0],
+        [-3.3124999999999996, 2059],
+        [-9.5, 2059],
+        [-9.5, 0],
+        [-3.3124999999999996, 0],
+      ],
+      id: "project.well_configuration_subject.fluids.fluid_subject00001",
+      name: "base sintetico",
+    },
+  ];
+  
+  export const drawPumpSequence = (fluid, color) => {
+    const shapesAllPumpSequence = [];
+    const allX0Y0 = [];
+    const allX1Y1 = [];
+    const allX0 = [];
+    const allX1 = [];
+    const allY0 = [];
+    const allY1 = [];
+  
+      /* For to create all x0 , y0 */
+    for (let index = 0; index < fluid.length; index += 1) {
+      if (index % 2 === 0) {
+        const testeX0Y0 = fluid[index][2];
+        allX0Y0.push(...testeX0Y0);
+      } else {
+        const testeX1Y1 = fluid[index][3];
+        allX1Y1.push(...testeX1Y1);
+      }
+    }
+  
+     /* For to create all x1 , y1 */
+  
+    for (let index = 0; index < allX0Y0.length; index += 1) {
+      if (index % 2 === 0) {
+        const X0 = allX0Y0[index];
+        const X1 = allX1Y1[index];
+        allX0.push(X0);
+        allX1.push(X1);
+      } else {
+        const Y0 = allX0Y0[index];
+        const Y1 = allX1Y1[index];
+        allY0.push(Y0);
+        allY1.push(Y1);
+      }
+    }
+  
+      /* For to create all shapes for pump sequence fluids */
+  
+    for (let index = 0; index < allX0.length; index += 1) {
+      const internalShape = {
+        type: "rect",
+  
+        x0: allX0[index],
+  
+        y0: allY0[index],
+  
+        x1: allX1[index],
+  
+        y1: allY1[index],
+  
+        line: {
+          color: color,
+          width: 2,
+        },
+        opacity: 0.8,
+        fillcolor: color,
+      };
+      shapesAllPumpSequence.push(internalShape);
+    }
+    return shapesAllPumpSequence;
+  };
+  
+  export const base_sintetica = pumped_fluids
+    .filter((index) => index.name === "base sintetico")
+    .map((index) => index.points);
+  export const colorBaseSintetica = "#ffab00"
+  export const pasta = pumped_fluids
+    .filter((index) => index.name === "pasta")
+    .map((index) => index.points);
+  export const colorPasta = "#b1b1b3"
+    
+    export const espacador = pumped_fluids
+    .filter((index) => index.name === "espacador")
+    .map((index) => index.points);
+  export const colorEspacador = "#fb0200"
+  
+  export const drawBaseSintetica = drawPumpSequence(base_sintetica,colorBaseSintetica);
+  
+  export const drawPasta = drawPumpSequence(pasta, colorPasta);
+  
+  export const drawEspacador = drawPumpSequence(espacador, colorEspacador);
