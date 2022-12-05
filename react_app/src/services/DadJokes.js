e253d0caad64d6ba66e6e07e50c5585fff9d10a4
ssribeirolmmp <sergio@lmmp.mec.puc-rio.br>
2022-12-05 10:28:18 -0300

    [update] specific service requests

---
+++ react_app/src/services/DadJokes.js
@@ -0,0 +1,20 @@
+// Import axios from specific API definition
+import axios from "../apis/dadJokesAPI";
+
+
+// GET Method Request for DadJokes API
+const getDadJokesRequest = {
+    axiosInstance: axios,
+    method: 'GET',
+    url: '/',
+    requesConfig: {
+        headers: {
+            'Content-Language': 'en-US'
+        }
+    }
+};
+
+export {
+    getDadJokesRequest,
+
+};
