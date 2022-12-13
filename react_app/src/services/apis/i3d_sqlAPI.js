94582bfdfe653964c684c7f850858964f31bdbba
Renan Freixo <renanfreixo@hotmail.com>
2022-12-13 11:44:17 -0300

    [update] change files

--- react_app/src/services/apis/i3d_sqlAPI.js
+++ react_app/src/services/apis/i3d_sqlAPI.js
@@ -1,10 +1,11 @@
 import axios from "axios";
 
 
-
+//Define API BASE_URL
+const BASE_URL = "http://139.82.19.75:5000/api/";
 // Create axios instance, with specif request props(headers, auth, body, etc...)
 export default axios.create({
-    baseURL: "http://localhost:5000/api/",
+    baseURL: BASE_URL,
     headers: {
         'Content-Type':'application/json',
         'Accept':'application/json'
