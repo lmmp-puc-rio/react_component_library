a557bf7275862227a0c1ae1eafac9cc70b212256
Tarcila <tarcilasg@gmail.com>
2022-12-22 10:13:32 -0300

    [fix] added onMouseEnter event instead onMouseOver

--- react_app/src/components/common/fab/FAB.jsx
+++ react_app/src/components/common/fab/FAB.jsx
@@ -16,8 +16,7 @@ function ActionButton(props) {
         tooltip={props.tooltip}
         style={props.styles || defaultItemStyles}
         onClick={props.onClick}
-        onMouseOver={props.onMouseOver}
-        onMouseLeave={props.onMouseLeave}
+        onMouseEnter={props.onMouseEnter}
       >
         <i className={props.icon} style={props.iconStyles}></i>
         {props.children}
@@ -29,93 +28,70 @@ function ActionButton(props) {
 // # Link SubComponent for FAB Construction
 function Action(props) {
   return (
-    <li>
-      <button
-        href={props.href}
-        className={"fab-action-button"}
-        style={props.styles || defaultItemStyles}
-      >
-        <span className="tooltip">{props.tooltip}</span>
-        <i className={props.icon} style={props.iconStyles}></i>
-        {props.children}
-      </button>
-    </li>
-  );
-  /* return (
     <a
       href={props.href}
       className={"fab-action-button"}
       style={props.styles || defaultItemStyles}
     >
-      <spam className="tooltip">{props.tooltip}</spam>
+      <span className="tooltip">{props.tooltip}</span>
       <i className={props.icon} style={props.iconStyles}></i>
       {props.children}
     </a>
-  ); */
+  );
 }
 
 // # FAB Component Construction
 function FAB(props) {
-  /* State responsible for expanding the main button after an action (onClik or onMouseOver) */
+  /* State responsible for expanding the main button after an action (onClik or onMouseEnter) */
   const [open, setOpen] = useState(false);
-  console.log("PROPS.DATA dentro da função FAB:", props.data);
-  const dataActions = props.data.actions;
-  console.log("PROPS.DATA.ACTIONS dentro da função FAB:", dataActions);
+  const data = props.data;
 
   function handleClick() {
     setOpen(!open);
   }
 
   const handleMouseEnter = (e) => {
-    console.log("entrou!");
     setOpen(!open);
-  };
-  const handleMouseLeave = (e) => {
-    console.log("saiu!");
-    setOpen(false);
   };
 
   // Tag Rendering
   return (
     /* Changing the class of the container, it changes the opening direction of the button */
-    <div className={props.data.actionButtom.direction} style={props.styles}>
+    <div className={data.actionButtom.direction} style={props.styles}>
       {/* CREATE MANUALY */}
       {/* {props.children}   */}
       {/* //Loop over Actions */}
       {/* CREATE AUTOMATICALY FROM DATA*/}
       {/* //Loop through Actions Array */}
       {/* //Main Action Buttom */}
-      <ul>
-        <ActionButton
-          tooltip={props.data.actionButtom.tooltip}
-          icon={props.data.actionButtom.icon}
-          key={props.data.actionButtom.key}
-          rotate={props.data.actionButtom.rotate}
-          styles={{
-            backgroundColor: props.data.actionButtom.backgroundColor,
-            color: props.data.actionButtom.color,
-          }}
-          onClick={handleClick}
-          onMouseOver={handleMouseEnter}
-          onMouseLeave={handleMouseLeave}
-        />
-        {dataActions.map(
-          (action, index) =>
-            // Minor Action Buttons
-            open && (
-              <Action
-                tooltip={action.tooltip}
-                icon={action.icon}
-                key={action.key}
-                href={action.url}
-                styles={{
-                  backgroundColor: action.backgroundColor,
-                  color: action.color,
-                }}
-              />
-            )
-        )}
-      </ul>
+      <ActionButton
+        tooltip={data.actionButtom.tooltip}
+        icon={data.actionButtom.icon}
+        key={data.actionButtom.key}
+        rotate={data.actionButtom.rotate}
+        styles={{
+          backgroundColor: data.actionButtom.backgroundColor,
+          color: data.actionButtom.color,
+        }}
+        onClick={handleClick}
+        onMouseEnter={handleMouseEnter}
+      />
+      {data.actions.map(
+        (action, index) =>
+          // Minor Action Buttons
+          open && (
+            <Action
+              tooltip={action.tooltip}
+              icon={action.icon}
+              key={action.key}
+              href={action.url}
+              styles={{
+                backgroundColor: action.backgroundColor,
+                color: action.color,
+              }}
+            />
+          )
+      )}
     </div>
   );
 }
