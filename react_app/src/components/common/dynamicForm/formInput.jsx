ea1fd4dcec7afb76d3d8ef46cb34632b3f023ed8
annatrajano <annatrajano95@gmail.com>
2023-01-12 13:38:44 -0300

    [fix] formInput - style

--- react_app/src/components/common/dynamicForm/formInput.jsx
+++ react_app/src/components/common/dynamicForm/formInput.jsx
@@ -8,36 +8,77 @@ import "./formInput.css";
 
 function FormInput(props) {
   const [focused, setFocused] = useState(false);
-  const { label, errorMessage, onChange, id, ...inputProps } = props;
+  const {
+    label,
+    errorMessage,
+    onChange,
+    id,
+    placeholder,
+    disabled,
+    rows,
+    ...inputProps
+  } = props;
 
   const handleFocus = (e) => {
     setFocused(true);
   };
 
-  return (
-    <div className="formInput">
-      {inputProps.type === "select" ? (
-        <select {...inputProps} onChange={onChange}>
+  if (inputProps.type === "select") {
+    return (
+      <div>
+        <label className="label-input-component">{placeholder}</label>
+        <select
+          {...inputProps}
+          onChange={onChange}
+          className="select-input-component"
+        >
           {inputProps.options.map((option) => (
-            <option value={option.value} onChange={onChange}>
+            <option
+              value={option.value}
+              onChange={onChange}
+              disabled={option.disabled}
+            >
               {option.label}
             </option>
           ))}
         </select>
-      ) : (
-        <>
-          <label>{label}</label>
-          <input
-            {...inputProps}
-            onChange={onChange}
-            onBlur={handleFocus}
-            focused={focused.toString()}
-          />
-          <span>{errorMessage}</span>
-        </>
-      )}
-    </div>
-  );
+      </div>
+    );
+  } else if (inputProps.type === "textarea") {
+    return (
+      <div>
+        <label className="label-input-component">{label}</label>
+        <textarea
+          {...inputProps}
+          onChange={onChange}
+          onBlur={handleFocus}
+          focused={focused.toString()}
+          placeholder={placeholder}
+          disabled={disabled}
+          rows={rows}
+          className="input-component textarea"
+        />
+        <span className="span-input-component">{errorMessage}</span>
+      </div>
+    );
+  } else {
+    return (
+      <div>
+        <label className="label-input-component">{label}</label>
+        <input
+          {...inputProps}
+          type={inputProps.type}
+          onChange={onChange}
+          onBlur={handleFocus}
+          focused={focused.toString()}
+          placeholder={placeholder}
+          disabled={disabled}
+          className="input-component"
+        />
+        <span className="span-input-component">{errorMessage}</span>
+      </div>
+    );
+  }
 }
 
 export default FormInput;
