d9bbf8f2fbe86f081de60e595229d0138dc4aa04
Paula Soares <paulafabianasoares@gmail.com>
2023-11-17 13:29:41 -0300

    [add] tabData example

---
+++ react_app/src/data/tabData.js
@@ -0,0 +1,27 @@
+/*Tabs Data*/
+export const dataTabSequenciaExternalGeometry = [
+  {
+    id: "Revestimento anterior",
+    description: "Revestimento anterior",
+    name: "Revestimento anterior",
+  },
+  {
+    id: "Poço aberto",
+    description: "Poço aberto",
+    name: "Poço aberto",
+  },
+];
+
+/*Tabs Data*/
+export const dataTabSequencia = [
+  {
+    id: "Parâmetros Básicos",
+    description: "Parâmetros Básicos",
+    name: "Parâmetros Básicos",
+  },
+  {
+    id: "Parâmetros Avançados",
+    description: "Parâmetros Avançados",
+    name: "Parâmetros Avançados",
+  },
+];
