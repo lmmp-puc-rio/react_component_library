6b12a4cda63c007063e66add4ac9b57f7e3a936e
Paula Soares <paulafabianasoares@gmail.com>
2023-11-17 14:25:21 -0300

    [add] children's components of page TabsComponents

---
+++ react_app/src/components/common/childrenTabs/index.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+export function ChildrenTabs() {
+  return (
+    <div>
+      <p>Tab content</p>
+    </div>
+  );
+}
+
+export function ChildrenTabs2() {
+  return (
+    <div>
+      <p>Children tab 2</p>
+    </div>
+  );
+}
