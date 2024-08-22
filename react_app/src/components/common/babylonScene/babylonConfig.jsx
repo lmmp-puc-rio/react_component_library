3e03b34d1b3aaf80a3583bfeeab8e9c009c9cb29
Lucas Constantino <lucas@lmmp.mec.puc-rio.br>
2024-08-22 15:03:38 -0300

    [update] Scene Configurations

--- react_app/src/components/common/babylonScene/babylonConfig.jsx
+++ react_app/src/components/common/babylonScene/babylonConfig.jsx
@@ -1,5 +1,6 @@
 import * as BABYLON from '@babylonjs/core';
 import '@babylonjs/loaders/glTF/2.0'; // Import the GLTF loader
+import * as GUI from '@babylonjs/gui'; // Import the GUI module
 
 const createBabylonEngine = (canvas) => {
   return new BABYLON.Engine(canvas, true);
@@ -15,45 +16,94 @@ const createBabylonScene = (engine, arcRotateCamera, replaceCameraLight, cameraC
   ambientLight.intensity = 1.0; // Adjust the ambient light intensity
 
   // Set the background color to white
-  scene.clearColor = new BABYLON.Color4(1, 1, 1, 1);
+  scene.clearColor = new BABYLON.Color4(0.9, 0.9, 0.9, 1);
+
+  // Get the camera and lock its translation
+  const camera = scene.activeCamera;
+  if (camera instanceof BABYLON.ArcRotateCamera) {
+    camera.panningSensibility = 0; // Disable panning (translation)
+    camera.useBouncingBehavior = false; // Disable bouncing behavior
+    camera.useAutoRotationBehavior = false; // Disable auto-rotation behavior
+  }
 
   return scene;
 };
 
 const loadGLBModel = (scene, url, alpha, center) => {
-    return new Promise((resolve, reject) => {
+  return new Promise((resolve, reject) => {
       BABYLON.SceneLoader.Append(url, "", scene, function () {
-        const mesh = scene.meshes[scene.meshes.length - 1]; // Assume the last mesh loaded is the target
+          const mesh = scene.meshes[scene.meshes.length - 1]; //Wireframe mesh
+          const meshTwo = scene.meshes[0]; //Fluid mesh
+
+          // Calculate center of mass and offset for mesh
+          const boundingInfo = mesh.getBoundingInfo();
+          const meshCenter = boundingInfo.boundingBox.center;
+          
+          // Calculate center of mass and offset for meshTwo
+          const boundingInfoTwo = meshTwo.getBoundingInfo();
+          const meshCenter2 = boundingInfoTwo.boundingBox.center;
+          const offsetTwo = new BABYLON.Vector3(meshCenter2.x, meshCenter2.y, meshCenter2.z);
+
+          const offset = new BABYLON.Vector3(meshCenter.x, meshCenter.y, meshCenter.z);
+          meshTwo.position.subtractInPlace(offset);
+
+          var aspect_ratio = 500;
+          var interval_number = 20;
+          var boundingBox = mesh.getBoundingInfo().boundingBox;
+          var min = boundingBox.minimumWorld;
+          var max = boundingBox.maximumWorld;
+          var distanceToBase = min.y - meshCenter.y;
+          var space = (max.y - min.y) / interval_number;
+
+          var edges = [];
+          for (var i = 0; i <= interval_number; i++) {
+              edges.push({
+                  position: new BABYLON.Vector3(- 2 * max.x, min.y + i * space + distanceToBase, 0),
+                  label: (min.y + i * space).toFixed(1) * aspect_ratio  + " m"
+              });
+          }
+
+          // Create a TransformNode to group the edge meshes
+          const edgeParent = new BABYLON.TransformNode("edgeParent", scene);
+
+          // Add edges as child nodes of edgeParent
+          edges.forEach(edge => {
+              var edgeMesh = BABYLON.MeshBuilder.CreateBox("edge", { size: 0.005 }, scene);
+              edgeMesh.position = edge.position;
+              edgeMesh.visibility = 0; // Make the edge mesh invisible
+              edgeMesh.parent = edgeParent; // Set the TransformNode as parent
+
+              var label = new GUI.TextBlock();
+              label.text = edge.label;
+              label.color = "black";
+              label.fontSize = 12;
+ 
+              label.textVerticalAlignment = GUI.Control.VERTICAL_ALIGNMENT_CENTER;
+
+              var advancedTexture = GUI.AdvancedDynamicTexture.CreateFullscreenUI("UI", true, scene);
+              advancedTexture.addControl(label);
+              label.linkWithMesh(edgeMesh); // Link label with the edge mesh
+          });
+
+          scene.onBeforeRenderObservable.add(() => {
+            // Align the edgeParent with the camera's rotation around the Y axis
+            const viewMatrix = scene.activeCamera.getViewMatrix();
+            const rotationMatrix = BABYLON.Matrix.Identity();
+            rotationMatrix.copyFrom(viewMatrix);
         
-        // Calculate center of mass
-        const boundingInfo = mesh.getBoundingInfo();
-        const meshCenter = boundingInfo.boundingBox.center;
-        const centerToUse = center || meshCenter;
+            // Extract the Y rotation from the view matrix
+            const yRotation = Math.atan2(-viewMatrix.m[8], viewMatrix.m[10]);
+        
+            // Apply the Y rotation to the edgeParent
+            edgeParent.rotation.y = yRotation;
+        });
 
-        // Calculate offset to move mesh to origin
-        const offset = new BABYLON.Vector3(0, centerToUse.z, 0);
-
-        // First rotation arround X axis
-        const rotationQuaternionX = mesh.rotationQuaternion = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.X, 3/2 * Math.PI);
-
-        // Second rotation arround Y axis
-        const rotationQuaternionY = BABYLON.Quaternion.RotationAxis(BABYLON.Axis.Y, Math.PI);
-        mesh.rotationQuaternion = rotationQuaternionY.multiply(rotationQuaternionX || BABYLON.Quaternion.Identity());
-
-        mesh.position.subtractInPlace(offset);
-
-        // Apply transparency
-        if (mesh.material) {
-          mesh.material.transparencyMode = alpha ? BABYLON.Material.MATERIAL_ALPHABLEND : BABYLON.Material.MATERIAL_OPAQUE;
-          mesh.material.alpha = alpha ? 0.01 : 1.0;
-          mesh.material.backFaceCulling = false;
-        }
-  
-        resolve({ mesh, center: meshCenter });
+          resolve({ mesh, center: meshCenter });
+          resolve({ meshTwo, center: meshCenter2 });
       }, null, function (scene, message) {
-        reject(new Error(message));
+          reject(new Error(message));
       });
-    });
-  };
+  });
+};
 
 export { createBabylonEngine, createBabylonScene, loadGLBModel };
